6391fbc5db9a7a965ef349d4a61ff04a201922cf
Escoreale <94carcon@gafe.molndal.se>
2023-09-07 15:13:38 +0200

    Enum test

---
+++ EnumTest1/src/main/java/org/example/Main.java
@@ -0,0 +1,36 @@
+package org.example;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+
+    public enum months {
+        januari,
+        february,
+        mars,
+        april,
+        juni,
+        juli,
+        augusti,
+        september,
+        oktober,
+        november,
+        december
+    }
+    public static void main(String[] args) {
+
+
+        months month = months.february;
+
+
+        if (month == months.february) {
+            // do something
+        }
+
+        int user = 2;
+        if (user == months.february.ordinal()){
+
+        }
+
+        }
+    }
